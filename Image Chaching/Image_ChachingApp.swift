8ed01acc0b0cff79fa826bded54182e7f4d065c5
Ady Sanjaya <adysanjaya@outlook.co.id>
2024-01-27 13:14:36 +0800

    Initial Commit

---
+++ Image Chaching/Image_ChachingApp.swift
@@ -0,0 +1,17 @@
+//
+//  Image_ChachingApp.swift
+//  Image Chaching
+//
+//  Created by Kioser PC on 27/01/24.
+//
+
+import SwiftUI
+
+@main
+struct Image_ChachingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Image ChachingUITests/Image_ChachingUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Image_ChachingUITestsLaunchTests.swift
+//  Image ChachingUITests
+//
+//  Created by Kioser PC on 27/01/24.
+//
+
+import XCTest
+
+final class Image_ChachingUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
